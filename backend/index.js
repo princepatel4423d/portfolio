94b55e342c25126bccfc9834e9b27dc665121705
Prince Patel <princep4423d@gmail.com>
2025-07-03 16:52:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 // Allow frontend origin
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['https://portfolio-backend-c0zf.onrender.com'];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
