fbe786ef75df9562036dc16347a3ef79eb821f7c
Prince Patel <princep4423d@gmail.com>
2025-07-03 15:52:39 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://portfolio-backend-c0zf.onrender.com',
   withCredentials: true, // VERY important for cookie-based auth
 });
 
